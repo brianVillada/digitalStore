138b76ee3c076095304b73f69f140c32618c32b6
Brian Villada <brian.villada@udea.edu.co>
2023-11-28 11:46:18 -0500

    initial commit

---
+++ js/carrito.js
@@ -0,0 +1,10 @@
+//variables globales
+let btn_carrito = document.querySelectorAll(".boton-carrito");
+let listaCarrito = document.querySelector(".listado-carrito tbody");
+let tablaCarrito = document.querySelector(".listado-carrito");
+let alerta = document.querySelector(".alerta");
+let bolsa = document.querySelector(".con-bolsa");
+let conPro = 0;
+
+
+
